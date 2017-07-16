76bad658f5cbc7ebb76b0578361cb899e4ad0720
Stephen Ward <stephen.ward21@gmail.com>
2017-07-16 15:53:26 -0400

    add img placeholders to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -38,7 +38,37 @@ class Home extends Component{
 	render(){
 		return(
 			<div>
-				<h1>Home Page</h1>
+				<h1>Models</h1>
+				<div className='container'>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='shop-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+				</div>
+				<br />
+				<div className='featured-models'>
+					<h2>Featured Products</h2>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+				</div>
+				<div className='featured-models'>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+				</div>
+				<div className='featured-models'>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+					<a className='featured-images' href="http://placeholder.com"><img src="http://via.placeholder.com/200x200" /></a>
+				</div>
+			
 			</div>
 		)
 	}
